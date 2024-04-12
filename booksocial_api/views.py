fda26a88af13d93b0be9816e78946ae469381ab4
adityavshahi <avshahi2001@gmail.com>
2024-04-12 19:32:08 +0530

    permissions integration

--- booksocial_api/views.py
+++ booksocial_api/views.py
@@ -2,9 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from booksocial_api import serializers
 from booksocial_api import models
+from booksocial_api import permissions
 
 
 class HealthCheckView(APIView):
@@ -50,3 +52,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)

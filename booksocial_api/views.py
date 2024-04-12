9447bed65de2967b2e9880980083af0fa020b532
adityavshahi <avshahi2001@gmail.com>
2024-04-12 19:39:26 +0530

    added search filter to profiles

--- booksocial_api/views.py
+++ booksocial_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from booksocial_api import serializers
 from booksocial_api import models
@@ -54,3 +55,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email')
